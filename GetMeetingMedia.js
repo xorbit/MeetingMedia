2205425a2e4513e944b7bbdec308a98ce48ca53c
Patrick Van Oosterwijck <patrick@silicognition.com>
2016-06-16 08:38:21 -0600

    Fix download failure due to absolute URL
    Before, I dumbly assumed that all media URLs were going to be relative to
    http://wol.jw.org.  Now that they fixed the video links to jw.org,
    this isn't the case anymore, so my simple string concatenation to
    create an absolute link would create invalid links.
    
    I tried to implement video loading, but a chain of redirects fails for
    'request' while it works in a browser.  I haven't yet figured out why.
    This causes all the video links to go to the jw.org homepage for
    my script, and they somehow pick up some world map images.

--- GetMeetingMedia.js
+++ GetMeetingMedia.js
@@ -25,11 +25,20 @@ function getSources(html) {
   return links;
 }
 
+// Create absolute URL (add prefix if not absolute)
+
+function absUrl(url) {
+  if (url.indexOf('http://') != 0 && url.indexOf('https://') != 0) {
+    url = 'http://wol.jw.org' + url;
+  }
+  return url;
+}
+
 // Get all media for the meeting
 
 function getMedia(lang, month, day, callback) {
-  request('http://wol.jw.org/' + lang + '/wol/dt/r1/lp-e/2016/' +
-          month + '/' + day, function (err, resp, html) {
+  request(absUrl('/' + lang + '/wol/dt/r1/lp-e/2016/' +
+          month + '/' + day), function (err, resp, html) {
     if (err) {
       callback(err);
       return;
@@ -39,7 +48,7 @@ function getMedia(lang, month, day, callback) {
     (function getSourceMedia(sources) {
       var source = sources.shift();
       if (source != undefined) {
-        request('http://wol.jw.org' + source[1], function(err, resp, html) {
+        request(absUrl(source[1]), function(err, resp, html) {
           if (err) {
             callback(err);
             return;
@@ -47,13 +56,39 @@ function getMedia(lang, month, day, callback) {
           // We use the jQuery-like cheerio module for this
           // Build a fake DOM and jQuery object
           var $ = cheerio.load(html);
-          // Get links to all figures
-          $('figure img').each(function (i, el) {
-            media.push([source[0], source[1], $(this).attr('alt'),
-                          $(this).attr('src')]);
-          });
-          // Get the next source's media
-          getSourceMedia(sources);
+          // See if this is a video link page
+          // Since we don't run the page Javascript, we need to use
+          // the noscript method, which involves another download page
+          // NOTE: This doesn't actually work right now, because for
+          // for some reason the chain of forwarding fails for
+          // request while it works in a browser
+          var vlink = $('div.jsIncludeVideo noscript a').attr('href');
+          // Is it a video link page?
+          if (vlink) {
+            // Load the video download page
+            request(absUrl(vlink), function(err, resp, html) {
+              if (err) {
+                callback(err);
+                return;
+              }
+              // Build a fake DOM and jQuery object
+              $ = cheerio.load(html);
+              // Get info for video
+              media.push([source[0], source[1],
+                    $('table td').has('a.aVideoURL').text().trim(),
+                    $('a.lnkDnld:contains(720p)').attr('href')]);
+              // Get the next source's media
+              getSourceMedia(sources);
+            });
+          } else {
+            // Get links to all figures
+            $('figure img').each(function (i, el) {
+              media.push([source[0], source[1], $(this).attr('alt'),
+                            $(this).attr('src')]);
+            });
+            // Get the next source's media
+            getSourceMedia(sources);
+          }
         });
       } else {
         media = _.uniqBy(media, function (x) { return x[3] });
@@ -70,12 +105,12 @@ var d = new Date();
 
 getMedia('en', d.getMonth() + 1, d.getDate(), function (err, media) {
   if (err) {
-    console.log(err);
     return;
   }
   for (var i = 0; i < media.length; i++) {
-    request('http://wol.jw.org' + media[i][3]).pipe(fs.createWriteStream(
+    request(absUrl(media[i][3])).pipe(fs.createWriteStream(
       _.padStart((i+1), 2, '0') + '. ' + media[i][0] + ' - ' +
       media[i][2] + '.jpg'));
   }
 });
+
