135652ed59e9b3fbc5ea8828bfe6ea0dec843f88
Patrick Van Oosterwijck <patrick@silicognition.com>
2017-01-04 13:15:03 -0700

    Fix for 2017, fix video download
    - There was some hard coded stuff for 2016 so it didn't correctly
    download the 2017 media.
    - Finally figured out why videos weren't downloading, it seems the
    jw.org requires a ckLang cookie to be set, otherwise it will
    redirect to /
    - Some minor fixes and code improvements + some more comments

--- GetMeetingMedia.js
+++ GetMeetingMedia.js
@@ -17,18 +17,21 @@ function getSources(html) {
       'div.docClass-WatchtowerTOC.pub-w16 a')
       .not('ul.noMarker a')
   .each(function (i, el) {
-    links.push([$(this).text(), $(this).attr('href')]);
+    links.push({
+      name: $(this).text(),
+      url: $(this).attr('href')
+    });
   });
-  links = _.uniqBy(links, function (x) { return x[1] });
-  links = _.filter(links, function (x) { return x[0] != '' });
-  links = _.filter(links, function (x) { return x[0][0] != '\n' });
+  // Filter out duplicates and things without a name
+  links = _.uniqBy(links, function (x) { return x.url });
+  links = _.filter(links, function (x) { return x.name != '' });
+  links = _.filter(links, function (x) { return x.name[0] != '\n' });
   return links;
 }
 
 // Create absolute URL (add prefix if not absolute)
 
 function absUrl(url) {
-console.log(url);
   if (url && url.indexOf('http://') != 0 && url.indexOf('https://') != 0) {
     url = 'http://wol.jw.org' + url;
   }
@@ -37,19 +40,30 @@ console.log(url);
 
 // Get all media for the meeting
 
-function getMedia(lang, month, day, callback) {
-  request(absUrl('/' + lang + '/wol/dt/r1/lp-e/2016/' +
+function getMedia(lang, year, month, day, callback) {
+  // We need a language cookie for stuff loaded from jw.org
+  var jar = request.jar();
+  jar.setCookie(request.cookie('ckLang=E'), 'https://www.jw.org');
+  var wolreq = request.defaults({ jar: jar });
+  // Get the schedule
+  wolreq(absUrl('/' + lang + '/wol/dt/r1/lp-e/' + year + '/' +
           month + '/' + day), function (err, resp, html) {
+    // Quit on error
     if (err) {
       callback(err);
       return;
     }
+    // Get the source article links
     var sources = getSources(html);
+    // Recursively get the media links from the articles
     var media = [];
     (function getSourceMedia(sources) {
+      // Get the next source
       var source = sources.shift();
+      // Check if we got one or we're done
       if (source != undefined) {
-        request(absUrl(source[1]), function(err, resp, html) {
+        wolreq(absUrl(source.url), function(err, resp, html) {
+          // Quit on error
           if (err) {
             callback(err);
             return;
@@ -60,14 +74,12 @@ function getMedia(lang, month, day, callback) {
           // See if this is a video link page
           // Since we don't run the page Javascript, we need to use
           // the noscript method, which involves another download page
-          // NOTE: This doesn't actually work right now, because for
-          // for some reason the chain of forwarding fails for
-          // request while it works in a browser
           var vlink = $('div.jsIncludeVideo noscript a').attr('href');
           // Is it a video link page?
           if (vlink) {
             // Load the video download page
-            request(absUrl(vlink), function(err, resp, html) {
+            wolreq(absUrl(vlink), function(err, resp, html) {
+              // Quit on error
               if (err) {
                 callback(err);
                 return;
@@ -75,25 +87,33 @@ function getMedia(lang, month, day, callback) {
               // Build a fake DOM and jQuery object
               $ = cheerio.load(html);
               // Get info for video
-              media.push([source[0], source[1],
-                    $('table td').has('a.aVideoURL').text().trim(),
-                    $('a.lnkDnld:contains(720p)').attr('href')]);
+              media.push({
+                source: source,
+                name: $('table td').has('a.aVideoURL').text().trim(),
+                url: $('a.linkDnld:contains(720p)').attr('href')
+              });
               // Get the next source's media
               getSourceMedia(sources);
             });
           } else {
             // Get links to all figures
             $('figure img').each(function (i, el) {
-              media.push([source[0], source[1], $(this).attr('alt'),
-                            $(this).attr('src')]);
+              media.push({
+                source: source,
+                name: $(this).attr('alt'),
+                url: $(this).attr('src')
+              });
             });
             // Get the next source's media
             getSourceMedia(sources);
           }
         });
       } else {
-        media = _.uniqBy(media, function (x) { return x[3] });
-        media = _.filter(media, function (x) { return x[2] != '' });
+        // Filter out duplicates and things without a URL or name
+        media = _.uniqBy(media, function (x) { return x.url });
+        media = _.filter(media, function (x) { return x.url });
+        media = _.filter(media, function (x) { return x.name != '' });
+        // Return the list of media
         callback(undefined, media);
       }
     })(sources);
@@ -104,15 +124,16 @@ function getMedia(lang, month, day, callback) {
 
 var d = new Date();
 
-getMedia('en', d.getMonth() + 1, d.getDate(), function (err, media) {
+getMedia('en', d.getFullYear(), d.getMonth() + 1, d.getDate(), function (err, media) {
   if (err) {
+    console.log(err);
     return;
   }
   for (var i = 0; i < media.length; i++) {
-    var url = absUrl(media[i][3]);
-    if (url) request(url).pipe(fs.createWriteStream(
-      _.padStart((i+1), 2, '0') + '. ' + media[i][0] + ' - ' +
-      media[i][2] + '.jpg'));
+    var m = media[i];
+    request(absUrl(m.url)).pipe(fs.createWriteStream(
+      _.padStart((i+1), 2, '0') + '. ' + media[i].source.name + ' - ' +
+      media[i].name));
   }
 });
 
